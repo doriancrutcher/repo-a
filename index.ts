7705a60de4665a1b8e8212288e5d050436a7e9fb
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2023-03-29 09:57:51 -0700

    added typescript files

---
+++ index.ts
@@ -0,0 +1,103 @@
+function a(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "a";
+}
+
+function b(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "b";
+}
+
+function c(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "c";
+}
+
+function d(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "d";
+}
+
+function e(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "e";
+}
+
+function f(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "f";
+}
+
+function g(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "g";
+}
+
+function h(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "h";
+}
+
+function i(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "i";
+}
+
+function j(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "j";
+}
+
+function k(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "k";
+}
+
+function l(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "l";
+}
+
+function m(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "m";
+}
+
+function n(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "n";
+}
+
+function o(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "o";
+}
+
+function p(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "p";
+}
+
+function q(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "q";
+}
+
+function r(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "r";
+}
+
+function s(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "s";
+}
+
+function t(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "t";
+}
+
+function u(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "u";
+}
+
+function v(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "v";
+}
+
+function w(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "w";
+}
+
+function x(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "x";
+}
+
+function y(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "y";
+}
+
+function z(str: string): boolean {
+  return str.charAt(0).toLowerCase() === "z";
+}
